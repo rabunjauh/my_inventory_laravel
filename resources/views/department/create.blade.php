30552948551e60ddcb63611c220dd43836a999ba
Mustafa <mustafa.visionet@gmail.com>
2022-11-21 20:33:17 +0700

    modify department/index add status column

---
+++ resources/views/department/create.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.main')
+
+@section('container')
+<div class="container">
+  <div class="row">
+    <h1>{{ $title }}</h1> 
+  </div>
+  <div class="row">
+    <div class="col-md-5">
+      <form action="/department" method="post">
+        @csrf
+      <div class="mb-3">
+        <label for="name" class="form-label">Department Name</label>
+        <input type="text" class="form-control @error('name')is-invalid @enderror" id="name" name="name" value="{{ old('name') }}" placeholder="Department Name" required autofocus>
+        @error('name')
+        <div class="invalid-feedback">
+          {{ $message }}
+        </div>
+        @enderror
+      </div>
+    </div>
+  </div>
+
+  <div class="row mt-3">
+    <div class="col-md-6">
+      <button type="submit" class="btn btn-primary">Save</button>
+      <button type="reset" class="btn btn-warning">Reset</button>
+      <a href="/department" class="btn btn-info ms-auto">Back</a>
+    </div>
+      </form>
+  </div>
+</div>
+@endsection
