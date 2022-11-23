8ea3c4c25ae4c082fa8e3a6a37bd9a03d7b974da
Mustafa <mustafa.visionet@gmail.com>
2022-11-23 19:39:50 +0700

    create function hardware in model Supplier

--- app/Models/Supplier.php
+++ app/Models/Supplier.php
@@ -10,4 +10,8 @@ class Supplier extends Model
     use HasFactory;
 
     protected $guarded = ['id'];
+
+    public function inventory() {
+        return $this->hasMany(Hardware::class);
+    }
 }
