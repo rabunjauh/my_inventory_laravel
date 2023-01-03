67b0f2d276ef97484b7f24d2bf00188909aad0ea
Mustafa <mustafa.visionet@gmail.com>
2023-01-03 09:12:26 +0700

    modify migration position(add column status)

--- database/migrations/2022_11_21_141137_create_positions_table.php
+++ database/migrations/2022_11_21_141137_create_positions_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->bigInteger('department_id');
+            $table->tinyInteger('status');
             $table->timestamps();
         });
     }
